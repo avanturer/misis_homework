e4949850d11675188edc1a7c9a01dfc759c031f8
avanturer <72664467+avanturer@users.noreply.github.com>
2022-10-04 22:25:03 +0300

    Update hw-05-01.cpp

--- orkin_r_r/hw-05-01.cpp
+++ orkin_r_r/hw-05-01.cpp
@@ -18,18 +18,19 @@ int main() {
     double d = 0.1;
     double e = 0.001;
     double f = 0;
-    for (a; a < b; a+=d){
-        double s1 = 0;
-        double s2 = 0;
-        for (long long int i = 0; i >= 0; i++){
-            s1 += (std::pow(-1, i))*((std::pow(a, 2*i)) / (fac_count(2*i)));
-            s2 += (std::pow(-1, i+1))*((std::pow(a, 2*(i+1))) / (fac_count(2*(i+1))));
+    for (a; a <= b; a += d) {
+        double s = 0;
+        double x = 0;
+        for (long long int i = 0; i >= 0; i++) {
+            x = (std::pow(-1, i)) * ((std::pow(a, 2 * i)) / (fac_count(2 * i)));
+            s += x;
             f = std::cos(a);
-            if (std::abs(s2-s1) < e){
-                std::cout<<a<<"|"<<s1<<"|"<<f<<std::endl;
+            if (std::abs(x) < e) {
+                std::cout << a << "|" << s << "|" << f << std::endl;
                 break;
             }
         }
+
     }
     return 0;
 }
