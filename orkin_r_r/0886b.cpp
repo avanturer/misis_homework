35dd078efedd4344ae1969af7ec5b9a641b52364
avanturer <72664467+avanturer@users.noreply.github.com>
2022-11-08 12:16:02 +0300

    Update and rename 0886b.cpp NET to 0886b.cpp

--- orkin_r_r/0886b.cpp
+++ orkin_r_r/0886b.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <algorithm>
 int main()
 {
     int n = 0;
     int a = 0;
+    int minimal = 10000000;
+    int result = 0;
     std::vector<int> arr;
-    
+    std::map<int, int> mp;
     std::cin>>n;
     
     for(int i = 0; i< n; i++){
         std::cin>>a;
         arr.push_back(a);
+        mp[a] = i;
     }
     if (n == 1){
         std::cout<<arr[0];
         return 0;
     }
-    for(int j = 0; j<n-1; j++){
-        bool flag = false;
-        flag = *std::find(std::begin(arr)+(j+1), std::end(arr), arr[j]);
-        if (flag == false){
-            std::cout<<arr[j];
-            break;
+    result = mp[0];
+    for(int i = 0; i<n; i++){
+        if(mp[arr[i]] < minimal){
+            result = arr[i];
+            minimal = mp[arr[i]];
         }
     }
+    std::cout<<result;
     return 0;
 }
