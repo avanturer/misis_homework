83afa813fef134564e48b07292ca9882001e3826
avanturer <72664467+avanturer@users.noreply.github.com>
2022-11-22 07:45:08 +0300

    add operator +-

--- orkin_r_r/vector_library.cpp
+++ orkin_r_r/vector_library.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
 
 struct Rdec2D{
     double x = 0.0;
@@ -12,52 +11,88 @@ struct Rpol2D{
     double phi = 0.0;
 };
 
+Rdec2D ToDec(const Rpol2D& a){
+    Rdec2D result;
+
+    result.x = a.z*std::cos(a.phi);
+    result.y = a.z*std::sin(a.phi);
+
+    return result;
+};
+
+Rpol2D ToPol(const Rdec2D& a){
+    Rpol2D result;
+
+    result.z = std::sqrt(std::pow(a.x,2)*std::pow(a.y,2));
+    result.phi = std::atan(a.y/a.x);
+
+    return result;
+
+};
+
 Rdec2D operator+(const Rdec2D& a, const Rdec2D& b){
     Rdec2D result;
-    
+
     result.x = a.x + b.x;
     result.y = a.y + b.y;
-    
+
     return result;
 };
 
 Rdec2D operator-(const Rdec2D& a, const Rdec2D& b){
     Rdec2D result;
-    
+
     result.x = a.x - b.x;
     result.y = a.y - b.y;
-    
+
+    return result;
+};
+
+Rpol2D operator+(const Rpol2D& a, const Rpol2D& b){
+    Rdec2D dec1;
+    Rdec2D dec2;
+    Rdec2D dec3;
+
+    Rpol2D result;
+
+    dec1 = ToDec(a);
+    dec2 = ToDec(b);
+
+    dec3.x = dec1.x + dec2.x;
+    dec3.y = dec1.y + dec2.y;
+
+    result = ToPol(dec3);
+
+    return result;
+};
+
+Rpol2D operator-(const Rpol2D& a, const Rpol2D& b){
+    Rdec2D dec1;
+    Rdec2D dec2;
+    Rdec2D dec3;
+
+    Rpol2D result;
+
+    dec1 = ToDec(a);
+    dec2 = ToDec(b);
+
+    dec3.x = dec1.x - dec2.x;
+    dec3.y = dec1.y - dec2.y;
+
+    result = ToPol(dec3);
+
     return result;
 };
 
 
 
-double PolToDec(double z, double phi){
-    double x = 0;
-    double y = 0;
-    x = z*std::cos(phi);
-    y = z*std::sin(phi);
-    
-    return x,y;
-};
-
-double DecToPol(double x, double y){
-    double z = 0;
-    double phi = 0;
-    
-    z = std::sqrt(std::pow(x,2)*std::pow(y,2));
-    phi = std::atan(y/x);
-    
-    return z, phi;
-    
-};
 
 int main()
 {
 
     Rdec2D dec;
     Rpol2D pol;
-    
+
     std::cout<<0;
     return 0;
 }
